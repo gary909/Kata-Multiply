db31a70b26d286763dfce70bfe133d2c69cc887c
Gary <gary909@gmail.com>
2020-07-18 12:01:28 +0200

    code

---
+++ index.js
@@ -0,0 +1,15 @@
+//The code does not execute properly. Try to figure out why.
+//From
+//https://www.codewars.com/kata/50654ddff44f800200000004/discuss
+/*
+function multiply(a, b) {
+    a * b
+}
+*/
+
+function multiply(a, b) {
+    return a * b;
+}
+
+//Hint 1: We're multiplying correctly, but why isn't the answer being returned?
+//Hint 2: We missing a return statement, without the multiplication isn't sent anywhere.
